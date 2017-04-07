147ba55ae4d728043b255f262a2c9612396fe014
hiho <hen03hat@gmail.com>
2017-04-07 22:05:29 +0300

    Switched from nuget js packages to npm packages

--- Web/SurveySystem.Web/App_Start/BundleConfig.cs
+++ Web/SurveySystem.Web/App_Start/BundleConfig.cs
@@ -4,7 +4,6 @@
 
     public static class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             RegisterScripts(bundles);
@@ -13,14 +12,36 @@
 
         private static void RegisterScripts(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            // TODO: remove all unnecessary node_modules files
+
+            bundles
+                .Add(new ScriptBundle("~/bundles/jquery")
+                .Include(FormatNodeModulePath("jquery/dist/jquery.min.js")));
+
+            bundles
+                .Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include(
+                    FormatNodeModulePath("jquery-validation/dist/jquery.validate.min.js"),
+                    FormatNodeModulePath("jquery-validation-unobtrusive/jquery.validate.unobtrusive.js")));
+
+            bundles
+                .Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include(FormatNodeModulePath("bootstrap/dist/js/bootstrap.min.js")));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles
+                .Add(new StyleBundle("~/Content/css")
+                .Include(
+                    FormatNodeModulePath("bootstrap/dist/css/bootstrap.min.css"),
+                    FormatNodeModulePath("bootstrap/dist/css/bootstrap-theme.min.css"),
+                    "~/Content/site.css"));
+        }
+
+        private static string FormatNodeModulePath(string path)
+        {
+            return $"~/node_modules/{path}";
         }
     }
 }
